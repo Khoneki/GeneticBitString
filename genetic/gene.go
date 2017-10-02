3265b1b18eb5a8478febf56ddc011e8d52b081a0
Khoneki <Khoneki@macbookProui-MacBook-Pro.local>
2017-10-03 02:18:21 +0900

    add 'gene.go' and 'generation.go'

---
+++ genetic/gene.go
@@ -0,0 +1,37 @@
+package genetic
+
+import "math/rand"
+
+type gene struct {
+	bit [8]int
+}
+
+func newGene() *gene {
+	g := gene{}
+	for i := 0; i < 8; i++ {
+		g.bit[i] = rand.Intn(2)
+	}
+	return &g
+}
+
+func (g gene) getFitness(goal [8]int) (fit int) {
+	fit = 0
+	for i := 0; i < 8; i++ {
+		if g.bit[i] == goal[i] {
+			fit++
+		}
+	}
+	return
+}
+
+func (g *gene) mutent() {
+	a := rand.Intn(8)
+	b := rand.Float64()
+	if b < 0.01 {
+		if g.bit[a] == 0 {
+			g.bit[a] = 1
+		} else {
+			g.bit[a] = 0
+		}
+	}
+}
---
+++ genetic/generation.go
@@ -0,0 +1,63 @@
+package genetic
+
+import "math/rand"
+
+type generation struct {
+	genes [1000]*gene
+	best  [4]*gene
+	goal  [8]int
+}
+
+func newGeneration(goal [8]int) *generation {
+	p := generation{}
+	var inv [8]int
+	for i := 0; i < len(goal); i++ {
+		if goal[i] == 0 {
+			inv[i] = 1
+		} else {
+			inv[i] = 0
+		}
+	}
+	for i := 0; i < len(p.genes); i++ {
+		p.genes[i] = newGene()
+	}
+	for i := 0; i < len(p.best); i++ {
+		p.best[i] = newGene()
+		p.best[i].bit = inv
+	}
+	p.goal = goal
+	return &p
+}
+
+func (p *generation) choice() {
+	for i := 0; i < len(p.genes); i++ {
+		for j := 0; j < len(p.best); j++ {
+			if p.genes[i].getFitness(p.goal) == p.best[j].getFitness(p.goal) {
+				p.best[j] = p.genes[i]
+				break
+			}
+		}
+	}
+}
+
+func (p generation) crossover() *generation {
+	new := newGeneration(p.goal)
+	for i := 0; i < len(p.genes); i++ {
+		p1 := rand.Intn(4)
+		p2 := rand.Intn(4)
+		a := rand.Intn(8)
+		for j := 0; j < a; j++ {
+			new.genes[i].bit[j] = p.best[p1].bit[j]
+		}
+		for j := a; j < 8; j++ {
+			new.genes[i].bit[j] = p.best[p2].bit[j]
+		}
+	}
+	return new
+}
+
+func (p generation) mutent() {
+	for i := 0; i < len(p.genes); i++ {
+		p.genes[i].mutent()
+	}
+}
