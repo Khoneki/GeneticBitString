9f1098e6e6043c5b5a294f91aab5042b9aba0453
Khoneki <Khoneki@macbookProui-MacBook-Pro.local>
2017-10-03 10:57:45 +0900

    change package of 'main.go'

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var goal = [8]int{0, 0, 0, 0, 1, 1, 1, 1}
+	parent := newGeneration(goal)
+	son := newGeneration(goal)
+	for parent.avgfit < 8 {
+		parent.choice()
+		fmt.Print(parent.avgfit)
+		son = parent.crossover()
+		son.mutent()
+		parent = son
+	}
+}
