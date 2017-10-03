b781bbdd63c2eda14b20d26285a5bfce70115cd5
Khoneki <woni8708@naver.com>
2017-10-03 12:46:23 +0900

    test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	var goal = [8]int{0, 1, 0, 0, 1, 0, 1, 1}
+	var goal = [8]int{0, 1, 0, 1, 1, 0, 1, 1}
 	parent := genetic.NewGeneration(goal)
 	son := genetic.NewGeneration(goal)
 	for i := 0; parent.Avgfit < 8; i++ {
