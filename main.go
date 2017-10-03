4b4d56e204dd7e3db0f2546f40e9e4c5664c038c
Khoneki <Khoneki@macbookProui-MacBook-Pro.local>
2017-10-03 11:17:35 +0900

    fixed

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/khoneki/GeneticBitString/genetic"
 )
 
 func main() {
 	var goal = [8]int{0, 0, 0, 0, 1, 1, 1, 1}
-	parent := newGeneration(goal)
-	son := newGeneration(goal)
+	parent := genetic.newGeneration(goal)
+	son := genetic.newGeneration(goal)
 	for parent.avgfit < 8 {
 		parent.choice()
 		fmt.Print(parent.avgfit)
